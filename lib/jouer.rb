6dade1e437bf10e830a64c46cc93b415467869c8
Omar Sahyoun <osahyoun@gmail.com>
2013-03-11 14:33:30 +0000

    Update redis connection params to work with heroku.

--- lib/jouer.rb
+++ lib/jouer.rb
@@ -8,10 +8,14 @@ require_relative 'jouer/game'
 require_relative 'jouer/view/view'
 require_relative 'jouer/view/league_table'
 
+
+
 module Jouer
   module Connection
+    REDIS_URI = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+
     def self.db
-      @db ||= Redis.new
+      @db ||= Redis.new(:host => REDIS_URI.host, :port => REDIS_URI.port, :password => REDIS_URI.password)
     end
   end
 end
